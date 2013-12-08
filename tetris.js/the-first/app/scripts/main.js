c1d7acd7c74fb954dd509d73e6fffe24c043cb7f
Aaron Sikes <jingadjanga@gmail.com>
2013-12-08 12:48:27 -0500

    Added a canvas tag and a raphael dep

--- tetris.js/the-first/app/scripts/main.js
+++ tetris.js/the-first/app/scripts/main.js
@@ -1,1 +1,8 @@
 console.log('\'Allo \'Allo!');
+var canvas = document.getElementById('tetris-canvas');
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = 'rgb(200,0,0)';
+ctx.fillRect(10, 10, 55, 50);
+
+ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+ctx.fillRect(30, 30, 55, 50);
