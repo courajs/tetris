a3db3a8ea0753fe0bbc7d84b471c808b1c62137f
Aaron Sikes <jingadjanga@gmail.com>
2013-12-08 16:01:37 -0500

    Added a key listener

--- tetris.js/the-first/app/scripts/main.js
+++ tetris.js/the-first/app/scripts/main.js
@@ -1,8 +1,12 @@
-console.log('\'Allo \'Allo!');
-var canvas = document.getElementById('tetris-canvas');
+var canvas = document.getElementById('tetris');
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = 'rgb(200,0,0)';
 ctx.fillRect(10, 10, 55, 50);
-
+ 
 ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
 ctx.fillRect(30, 30, 55, 50);
+
+$(document).on('keydown', function(event){
+    var key = vkey[event.keyCode];
+    $('#current-key').text(key);
+});
