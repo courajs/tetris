8b97cdda49ceae69056817c8b1e2baa35490507e
Aaron Sikes <jingadjanga@gmail.com>
2014-01-26 17:35:29 -0500

    Added some code for moving a piece downward

--- tetris.js/the-first/app/scripts/Piece.js
+++ tetris.js/the-first/app/scripts/Piece.js
@@ -1,6 +1,7 @@
 function Piece(tiles, color){
 	this.tiles = tiles;
 	this.color = color;
+	this.stuck = false;
 }
 
 Piece.prototype.copy = function(){
@@ -42,6 +43,7 @@ Piece.prototype.move_left = function(){
 }
 
 Piece.prototype.try_right = function(){
+	console.assert(!this.stuck, 'Calling try_right on an already stuck piece');
 	var righter = this.copy();
 	righter.move_right();
 	if (righter.is_valid()){
@@ -52,6 +54,7 @@ Piece.prototype.try_right = function(){
 }
 
 Piece.prototype.try_left = function(){
+	console.assert(!this.stuck, 'Calling try_left on an already stuck piece');
 	var lefter = this.copy();
 	lefter.move_left();
 	if (lefter.is_valid()){
@@ -61,14 +64,32 @@ Piece.prototype.try_left = function(){
 	}
 }
 
+Piece.prototype.fall = function(){
+	console.assert(!this.stuck, 'Calling fall on an already stuck piece');
+	var downer = this.copy();
+	downer.move_down();
+	if (downer.is_valid()){
+		this.clear();
+		this.tiles = downer.tiles;
+		this.stamp();
+		return true;
+	}
+	else {
+		this.stick();
+		return false;
+	}
+}
+
 
 
 Piece.prototype.stick = function(){
 	console.assert(this.is_valid(), 'Trying to stick an invalid piece');
+	console.assert(!this.stuck, 'You already stuck this piece!');
 	var color = this.color;
 	this.tiles.forEach(function(tile){
 		tile.stick(color);
 	});
+	this.stuck = true;
 };
 
 Piece.prototype.stamp = function(){
--- tetris.js/the-first/app/scripts/main.js
+++ tetris.js/the-first/app/scripts/main.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 var $tetris = $('#tetris');
 
 var columns = 300 / 50;
@@ -12,7 +7,6 @@ var board = [];
 for (var i = 0; i < columns; i++){
 	board.push([]);
 }
-
 
 for(var i = 0; i < rows; i++){
 	for (var j = 0; j < columns; j++){
@@ -24,20 +18,7 @@ for(var i = 0; i < rows; i++){
 	}
 }
 
-function setSomeGuy(){
-	board.random().random().color = colors.random();
-}
-
-
-$tetris.on('click', '.cell', function(event){
-	console.log('click');
-	$(this).toggleClass('clicked');
-})
-
-
-var handlers = {
-	'J': setSomeGuy
-}
+var handlers = {}
 
 $(document).on('keydown', function(event){
     var key = vkey[event.keyCode];
@@ -46,11 +27,14 @@ $(document).on('keydown', function(event){
 });
 
 
-var testPiece = new Piece([new Tile(1,1), new Tile(2,1), new Tile(2,2), new Tile(2,3)], blue);
-testPiece.stamp();
+var test_piece = new Piece([new Tile(1,1), new Tile(2,1), new Tile(2,2), new Tile(2,3)], blue);
+test_piece.stamp();
 handlers['<right>'] = function(){
-	testPiece.try_right();
+	test_piece.try_right();
 };
 handlers['<left>'] = function(){
-	testPiece.try_left();
+	test_piece.try_left();
+}
+handlers['<down>'] = function(){
+	test_piece.fall();
 }
